be306bb41950db7065ecbfe20dd3940bcaed29b8
Stephen M. Cameron <stephenmcameron@gmail.com>
2023-05-21 09:42:53 -0400

    Try drawing some lines
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- browzer-tanx.c
+++ browzer-tanx.c
@@ -21,6 +21,7 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <stdint.h>
 #include <SDL.h>
 #ifdef BTWASM
 #include <emscripten.h>
@@ -28,12 +29,24 @@
 
 #define UNUSED __attribute__((unused))
 
+#define SCREEN_XDIM 640
+#define SCREEN_YDIM 480
+
+static SDL_Window *window;
+static SDL_Renderer *renderer;
+static SDL_Surface *surface;
+
 void main_loop(void)
 {
-
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+	SDL_RenderDrawLine(renderer, 0, 0, SCREEN_XDIM - 1, SCREEN_YDIM - 1);
+	SDL_RenderDrawLine(renderer, 0, SCREEN_YDIM, SCREEN_XDIM - 1, 0);
+	SDL_RenderPresent(renderer);
 }
 
-int main(UNUSED int argc, UNUSED char *argv[])
+static int init_sdl2(void)
 {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		fprintf(stderr, "Unable to initialize SDL (Video):  %s\n", SDL_GetError());
@@ -43,6 +56,22 @@ int main(UNUSED int argc, UNUSED char *argv[])
 		fprintf(stderr, "Unable to initialize SDL (Events):  %s\n", SDL_GetError());
 		return 1;
 	}
+	if (SDL_CreateWindowAndRenderer(SCREEN_XDIM, SCREEN_YDIM, 0, &window, &renderer) != 0) {
+		fprintf(stderr, "Unable to create window/renderer: %s\n", SDL_GetError());
+		return 1;
+	}
+	surface = SDL_CreateRGBSurface(0, SCREEN_XDIM, SCREEN_YDIM, 32, 0, 0, 0, 0);
+	if (!surface) {
+		fprintf(stderr, "Unable to create RGB surface: %s\n", SDL_GetError());
+		return 1;
+	}
+	return 0;
+}
+
+int main(UNUSED int argc, UNUSED char *argv[])
+{
+	if (init_sdl2())
+		return -1;
 #ifdef BTWASM
 	emscripten_set_main_loop(main_loop, 60, 1);
 #else
