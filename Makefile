52181c5e61fadb75a82cdc40db5ae3e2d6722092
Stephen M. Cameron <stephenmcameron@gmail.com>
2023-05-21 08:03:11 -0400

    Add Makefile and browzer-tanx.c
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

---
+++ Makefile
@@ -0,0 +1,4 @@
+
+browzer-tanx:	browzer-tanx.c
+	gcc -O3 -Wall -Wextra -Wstrict-prototypes -o browzer-tanx browzer-tanx.c
+
---
+++ browzer-tanx.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#define UNUSED __attribute__((unused))
+
+int main(UNUSED int argc, UNUSED char *argv[])
+{
+	printf("Browzer-Tanx!\n");
+}
+
